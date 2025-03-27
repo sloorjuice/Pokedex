d26b43f8e6f064af75709e483548a02b222d7fe3
sloorjuice <antant8085@gmail.com>
2025-03-26 21:27:44 -0400

    Fixed name format

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -17,17 +17,20 @@ function PokeCard({ pokemon }) {
     const sprite = pokemon.sprites?.other?.['official-artwork']?.front_default || 'placeholder-image-url';
     const types = pokemon.types?.map((type) => type.type.name).join(', ') || 'Unknown';
 
+    // Replace dashes with spaces in the Pokémon name
+    const formattedName = pokemon.name.replace(/-/g, ' ');
+
     return(
         <Link to={`${pokemon.id}`}>
             <div className="movie-card">
                 <div className="movie-poster">
-                    <img src={sprite} alt={pokemon.name} />
+                    <img src={sprite} alt={formattedName} />
                 </div>
                 <div className="movie-overlay">
                     <button className={`favorite-btn ${favorite ? "active" : ''}`} onClick={onFavoriteClick}>♡</button>
                 </div>
             <div className="movie-info">
-                <h3>{pokemon.name}</h3>
+                <h3>{formattedName}</h3>
                 <p>{types}</p>
             </div>
             

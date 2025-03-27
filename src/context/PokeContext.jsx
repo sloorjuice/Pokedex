f8084af65cfa1238809a59985bec7fbee7b5d071
sloorjuice <antant8085@gmail.com>
2025-03-26 21:06:03 -0400

    Favorited pokemon are now saved

--- src/context/PokeContext.jsx
+++ src/context/PokeContext.jsx
@@ -1,41 +1,45 @@
 import { createContext, useEffect, useContext, useState } from "react";
 
-const PokeContext = createContext()
+const PokeContext = createContext();
 
-export const usePokeContext = () => useContext(PokeContext)
+export const usePokeContext = () => useContext(PokeContext);
 
-export const PokeProvider =({ children }) => {
-    const [favorites, setFavorites ] = useState([])
+export const PokeProvider = ({ children }) => {
+    const [favorites, setFavorites] = useState([]);
 
+    // Load favorites from localStorage on initial render
     useEffect(() => {
-        const storedFavs = localStorage.getItem('favorites')
+        const storedFavs = localStorage.getItem("favorites");
+        if (storedFavs) setFavorites(JSON.parse(storedFavs));
+    }, []);
 
-        if (storedFavs) setFavorites(JSON.parse(storedFavs))
-    },[])
+    // Save favorites to localStorage whenever they change
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    }, [favorites]);
 
     const addToFavorites = (pokemon) => {
-        setFavorites((prev => [...prev, pokemon]))
-    }
+        setFavorites((prev) => [...prev, pokemon]);
+    };
 
     const removeFromFavorites = (pokeId) => {
-        setFavorites(prev => prev.filter(pokemon => pokemon.id !==pokeId))
-    }
+        setFavorites((prev) => prev.filter((pokemon) => pokemon.id !== pokeId));
+    };
 
     const isFavorite = (pokeId) => {
-        return favorites.some(pokemon => pokemon.id === pokeId)
-    }
+        return favorites.some((pokemon) => pokemon.id === pokeId);
+    };
 
     const value = {
         favorites,
         addToFavorites,
         removeFromFavorites,
-        isFavorite
-    }
+        isFavorite,
+    };
 
     return (
         <PokeContext.Provider value={value}>
             {children}
         </PokeContext.Provider>
-    )
-
-}
+    );
+};

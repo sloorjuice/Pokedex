f0a7a49e851cb0f6c7f294cbf8cae1a17ccfbfd2
sloorjuice <antant8085@gmail.com>
2025-03-26 20:08:57 -0400

    Added buy me a coffee feature

--- Backend/src/components/Navbar.jsx
+++ Backend/src/components/Navbar.jsx
@@ -11,7 +11,6 @@ function Navbar() {
             <div className="navbar-links">
                 <Link to="/">Home</Link>
                 <Link to="/favorites">Favorites</Link>
-                <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="sloorjuice" data-description="Support me on Buy me a coffee!" data-message="Hey!" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
             </div>
         </nav>
     );
